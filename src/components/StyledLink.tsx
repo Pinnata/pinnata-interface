c2233e2fb76af4e879899c39669578d6f3f8129b
Amin <amineghennou3@gmail.com>
2021-09-12 00:56:21 +0600

    Fresh-File

--- src/components/StyledLink.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "@emotion/styled";
-import { Link } from "react-router-dom";
-
-export const StyledLink = styled(Link)({
-  height: "fit-content",
-  textDecoration: "none",
-  color: "var(--theme-ui-colors-primaryText)",
-});
--- src/utils/eth.ts
+++
@@ -1,7 +0,0 @@
-import { fromWei } from "web3-utils";
-import BN from "bn.js";
-import { humanFriendlyNumber } from "src/utils/number";
-
-export const humanFriendlyWei = (wei: BN | string) => {
-  return humanFriendlyNumber(fromWei(wei));
-};
