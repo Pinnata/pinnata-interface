1d6977fb25c7572c85f78fd0fc00ede7d3f8469e
Maruf <76469292+Mohamed-Amine-G@users.noreply.github.com>
2021-09-12 16:28:14 +0600

    App Installable

--- src/pages/Earn/Earn.tsx
+++ src/pages/Earn/Earn.tsx
@@ -1,22 +1,17 @@
 import React from 'react';
-import { EarnData } from '../../Data/EarnData';
-import { Link } from 'react-router-dom';
-import Image from '../../images/background.png';
-
+import Image from 'src/images/background.png';
+import { COLLATERAL_TOKENS } from "src/config"
+import { EarnEntry } from "src/pages/Earn/EarnEntry"; 
+import { css } from '@emotion/react';
 import {
-  Wrapper,
-  Container,
-  Title,
+  Container, 
+  Title, 
   Description,
-  CardContainer,
-  SecondTitle,
-  Flex,
-  Button,
-  InfoContainer,
-  Header,
-  Item,
+  Wrapper, 
   Background,
-} from './EarnStyles';
+  CardContainer,
+} from "src/components/MainPages";
+import { SimpleTable } from 'src/components/SimpleTable';
 
 const Earn = () => {
   return (
@@ -26,40 +21,32 @@ const Earn = () => {
         <Description>Lend assets for the highest yield.</Description>
       </Container>
       <CardContainer>
-        <SecondTitle>All active pools</SecondTitle>
-        <Flex>
-          <Button className='active'>All</Button>
-        </Flex>
-        <InfoContainer>
-          <Header>
-            <h2>Pool</h2>
-            <h2 className='center'>Projected APY %</h2>
-            <h2 className='center'>Total Supply</h2>
-            <h2 className='center'>Total Borrow</h2>
-            <h2 className='center'>Utilization</h2>
-            <h2 className='center'>Balance</h2>
-            <div></div>
-          </Header>
-          {EarnData.map((item, index) => {
-            return (
-              <Item key={index}>
-                <div className='flexFirst'>
-                  <img src={item.image1} alt='eth' className='image' />
-                  <h2>{item.firstTitle}</h2>
-                </div>
-                <div className='centerMiddle thin'>{item.apy}</div>
-                <div className='centerMiddle thin'>{item.supply}</div>
-                <div className='centerMiddle thin'>{item.borrow}</div>
-                <div className='centerMiddle thin'>{item.utilization}</div>
-                <div className='centerMiddle thin'>{item.balance}</div>
-                <div className='end'>
-                  <Link to='deposit'>Supply</Link>
-                  <Link to='withdrawn'>Withdrawn</Link>
-                </div>
-              </Item>
-            );
-          })}
-        </InfoContainer>
+        {/* <InfoContainer> */}
+        <SimpleTable>
+          <thead>
+            <tr>
+              <th
+                css={css`
+                  text-align: left !important;
+                `}
+              >
+                Asset
+              </th>
+              <th>APY</th>
+              <th>Total Supply</th>
+              <th>Total Borrow</th>
+              <th>Utilization</th>
+              <th>Balance</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {COLLATERAL_TOKENS.map((token) => (
+              <EarnEntry key={token.address} token={token}/>
+            ))}
+          </tbody>
+        </SimpleTable>
+        {/* </InfoContainer> */}
       </CardContainer>
       <Background src={Image} alt='Background Logo' />
     </Wrapper>
