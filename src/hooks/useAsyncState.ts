2d05b40f69d196792e4fc527d5706db275a27ffa
Samay Shamdasani <techboss302@gmail.com>
2021-11-02 23:09:31 -0400

    .

--- src/hooks/useAsyncState.ts
+++ src/hooks/useAsyncState.ts
@@ -2,18 +2,29 @@ import { useEffect, useState } from "react";
 
 export function useAsyncState<T>(
   initialState: T,
-  asyncGetter: () => Promise<T> | undefined
+  asyncGetter: () => Promise<T> | undefined,
+  localStorageKey?: string | undefined
 ): [T, () => void] {
   const [state, setState] = useState<T>(initialState);
   const [dirty, setDirty] = useState<boolean>(true);
   useEffect(() => {
+    if (localStorageKey) {
+      const localStorageValue = localStorage.getItem(localStorageKey);
+      if (localStorageValue) {
+        setState(JSON.parse(localStorageValue));
+      }
+    }
+
     asyncGetter()
       ?.then((v) => {
         setState(v);
+        if (localStorageKey) {
+          localStorage.setItem(localStorageKey, JSON.stringify(v));
+        }
         setDirty(false);
       })
       .catch(console.warn);
-  }, [asyncGetter, dirty]);
+  }, [asyncGetter, dirty, localStorageKey]);
 
   const refetch = () => {
     setDirty(true);
